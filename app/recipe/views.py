e7aa9bc9f801844fd86472143f32b05198113ebb
sstewart424 <sstewart424@gmail.com>
2021-04-05 14:27:48 +0100

    add changes to views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,7 +18,16 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create a new object"""
@@ -49,17 +58,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return [int(str_id) for str_id in qs.split(',')]
 
     def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        assigned_only = bool(
-            int(self.request.query_params.get('assigned_only', 0))
-        )
+        """Retrieve the recipes for the authenticated user"""
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
         queryset = self.queryset
-        if assigned_only:
-            queryset = queryset.filter(recipe__isnull=False)
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
 
-        return queryset.filter(
-            user=self.request.user
-        ).order_by('-name').distinct()
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
